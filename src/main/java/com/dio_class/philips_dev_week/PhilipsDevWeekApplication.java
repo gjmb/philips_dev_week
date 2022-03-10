dcbf2935d87867801e3fa0a624be811ac61a0e9f
gjmb <gjmbarbosa@gmail.com>
2022-03-10 14:36:34 -0300

    Inicializa o projeto

---
+++ src/main/java/com/dio_class/philips_dev_week/PhilipsDevWeekApplication.java
@@ -0,0 +1,13 @@
+package com.dio_class.philips_dev_week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhilipsDevWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhilipsDevWeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio_class/philips_dev_week/PhilipsDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio_class.philips_dev_week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhilipsDevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
